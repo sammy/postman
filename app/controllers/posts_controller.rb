f4e7f82d56619d502e4ec20f10b17cf57adeb094
Apostolos Samatas <flouts@gmail.com>
2013-11-03 21:04:40 +0100

    crud functionality complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,51 @@
 class PostsController < ApplicationController
+  before_action :set_post, :only => [:show, :edit]
   
   def index
     @posts = Post.all
   end
 
   def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Post successfully updated"
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def post_params
+    params.require(:post).permit!
+  end
+
+  def set_post
     @post = Post.find(params[:id])
   end
+
 
 end
