e0b7f156f36ee59af9c600c1af10bca87a789095
Apostolos Samatas <flouts@gmail.com>
2013-11-04 00:06:35 +0100

    categories views-forms and layout changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,32 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category has been created"
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit!
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
 end
