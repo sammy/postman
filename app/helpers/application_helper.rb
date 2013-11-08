1ed9f7de97148e705b9944c8339b9ee41165099e
Apostolos Samatas <flouts@gmail.com>
2013-11-08 23:34:02 +0100

    fixed date display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
     url.starts_with?('http://') ? url : "http://#{url}" 
   end
 
+  def datestamp(obj)
+    from_time = Time.now
+    distance_of_time_in_words(from_time,obj.created_at)
+  end
+
 end
